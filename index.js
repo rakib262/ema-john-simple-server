dcaef406e9f9b43627c52b48e6217d17a1cfd8dd
Rakib Khan Anjon <91784116+rakib262@users.noreply.github.com>
2022-02-14 13:00:21 +0600

    ema john simple server update

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const uri =`mongodb+srv://${(process.env.S3_BUCKET)}:${(process.env.SECRET_KEY)}
 
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('db working')
-})
-
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
